40a0ba917ff0e9e1f5339ec90547abed848c7cb6
Nonoo <nonoo@nonoo.hu>
2014-03-25 16:32:18 +0000

    init

---
+++ nbackup.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+scriptname=`basename $0`
+scriptdir=${0/$scriptname/}
+
+source $scriptdir/config
+source $redirectlogscript
+
+if [ "$1" = "quiet" ]; then
+	quietmode=1
+	redirectlog
+fi
+
+backup() {
+	src=$1
+	dst=$2
+
+	echo "*** backing up $src to $dst"
+
+	if [ $rsync -eq 1 ]; then
+		dryrunparam=
+		if [ $dryrunrsync -eq 1 ]; then
+			dryrunparam="--dry-run"
+		fi
+
+		#ssh -p $sshport $dsthost "[ ! -d $dst ]"
+		#direxists=$?
+		#if [ "$direxists" = "0" ]; then
+			echo "*** running rsync"
+			rsync $dryrunparam --verbose --compress-level=9 --archive \
+				--rsh "ssh -p $sshport" --progress --ignore-errors --delete $src $dsthost:$dst
+		#fi
+	fi
+
+	if [ $rdiffbackup -eq 1 ]; then
+		remoteschema="ssh -C -p $sshport %s rdiff-backup --server"
+
+		forceparam=
+		if [ $rdiffbackupforce -eq 1 ]; then
+			forceparam="--force"
+		fi
+
+		echo "*** running rdiff-backup"
+		rdiff-backup $forceparam --remote-schema "$remoteschema" $src $dsthost::$dst
+		echo "*** running rdiff-backup, listing increments"
+		rdiff-backup --remote-schema "$remoteschema" --list-increments $dsthost::$dst
+		echo "*** running rdiff-backup, removing older increments"
+		rdiff-backup --remote-schema "$remoteschema" --remove-older-than $removeoldertime --force $dsthost::$dst
+	fi
+
+	echo "*** backing up $src to $dst finished."
+}
+
+backup /etc/ $dstpath/etc
